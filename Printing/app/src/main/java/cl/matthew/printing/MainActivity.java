f2c95788bc1b75044149aff92e7ea0c3eeaa9efd
matthew <matthew.hss@hotmail.com>
2017-05-19 13:36:12 -0400

    modificada la versión del SDKCompiler

--- Printing/app/src/main/java/cl/matthew/printing/MainActivity.java
+++ Printing/app/src/main/java/cl/matthew/printing/MainActivity.java
@@ -1,11 +1,12 @@
 package cl.matthew.printing;
 
-import android.app.Activity;
+
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.R;
 
 import java.util.HashMap;
 
@@ -13,26 +14,27 @@ import comm.CommEntity;
 import comm.entity.BasePrinter;
 import common.utils.IOUtils;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnPrint = (Button) findViewById(android.R.id.btnPrint);
+        Button btnPrint = (Button) findViewById(R.id.btnPrint);
         btnPrint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 try {
                     String textModel = new String(IOUtils.readResource("assets/model/voucher_inst.txt"));
                     HashMap<String, String> hsmpParam = new HashMap<String, String>();
-                    EditText textToPrint = (EditText) findViewById(android.R.id.txtToPrint);
+                    EditText textToPrint = (EditText) findViewById(R.id.txtToPrint);
                     //hsmpParam.put("$BARCODE$","123456789012");
                     hsmpParam.put("$BARCODE$", textToPrint.toString());
                     BasePrinter basePrinter = CommEntity.getPrinter(1);
                     basePrinter.print(textModel, hsmpParam);
                 } catch (Exception e) {
+                    Log.e("Impresora: ", "Error", e);
                 }
             }
         });
