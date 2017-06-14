4126bd6ec3a4b842daef5ac8e9bd18f7bc7abd8e
Shinkiro <xxdragonxeroxx@gmail.com>
2017-06-14 10:02:02 -0400

    Rearrange MainActivity for readability, added QR generation, Added imageview for QR

--- TestingPrint/app/src/main/java/com/example/matthew/testingprint/MainActivity.java
+++ TestingPrint/app/src/main/java/com/example/matthew/testingprint/MainActivity.java
@@ -1,12 +1,29 @@
 package com.example.matthew.testingprint;
 
 
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
 import java.util.HashMap;
 
 import comm.CommEntity;
@@ -15,32 +32,30 @@ import common.utils.IOUtils;
 
 public class MainActivity extends AppCompatActivity {
 
-    private boolean inited = false;
+    public final static int QRcodeWidth = 500 ;
+    private static final String IMAGE_DIRECTORY = "/QRcodeDemonuts";
+    Bitmap bitmap;
+
+    EditText data = (EditText) findViewById(R.id.textdata);
+    ImageView qr = (ImageView) findViewById(R.id.qrfield);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        String textModel = new String(IOUtils.readResource("assets/model/voucher_inst.txt"));
-//        HashMap<String, String> hsmpParam = new HashMap<String, String>();
-//        //EditText textToPrint = (EditText) findViewById(R.id.txtToPrint);
-//        hsmpParam.put("$BARCODE$","123456789012");
-//        //hsmpParam.put("$BARCODE$", textToPrint.toString());
-//        BasePrinter basePrinter = CommEntity.getPrinter(1);
-//        basePrinter.print(textModel, hsmpParam);
-
         CommEntity.init();
 
-        Button btnPrint = (Button) findViewById(R.id.btnPrint);
+
+        /*
         btnPrint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //try {
-                String textModel = new String(IOUtils.readResource("assets/model/voucher_inst.txt"));
+                String textModel = new String(IOUtils.readResource("assets/model/voucher_qr.txt"));
                 HashMap<String, String> hsmpParam = new HashMap<String, String>();
-                //EditText textToPrint = (EditText) findViewById(R.id.txtToPrint);
-                hsmpParam.put("$BARCODE$", "123456789012");
+                EditText textToPrint = (EditText) findViewById(R.id.txtToPrint);
+                hsmpParam.put("$QRCODE$", "Puntos Obtenidos: "+ textToPrint.getText().toString());
                 //hsmpParam.put("$BARCODE$", textToPrint.toString());
                 BasePrinter basePrinter = CommEntity.getPrinter(1);
                 basePrinter.print(textModel, hsmpParam);
@@ -50,7 +65,102 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+        */
     }
 
+    public void createQR (View view) {
 
+        if(data.getText().toString().trim().length() == 0) {
+            Toast.makeText(MainActivity.this, "Enter String!", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            try {
+                bitmap =  TextToImageEncode(data.getText().toString());
+                qr.setImageBitmap(bitmap);
+                String path = saveImage(bitmap);
+                Toast.makeText(MainActivity.this, "QR creado en  => "+ path, Toast.LENGTH_SHORT).show();
+            }
+            catch (WriterException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printQR (View view){
+        String textModel = new String(IOUtils.readResource("assets/model/voucher_inst.txt"));
+        HashMap<String, String> hsmpParam = new HashMap<>();
+        String info = data.getText().toString().trim();
+        //Formato par enviar el QR mostrado
+        //TODO: Find a way to send to the printer.
+        //Bitmap bmap = qr.getDrawingCache();
+        hsmpParam.put("$QRDATA$", info);
+        BasePrinter basePrinter = CommEntity.getPrinter(1);
+        basePrinter.print(textModel, hsmpParam);
+    }
+
+    //Metodos para crear el QR y luego mostarlo en la pantalla.
+    public String saveImage(Bitmap myBitmap) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        myBitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File wallpaperDirectory = new File(
+                Environment.getExternalStorageDirectory() + IMAGE_DIRECTORY);
+        // have the object build the directory structure, if needed.
+
+        if (!wallpaperDirectory.exists()) {
+            Log.d("dirrrrrr", "" + wallpaperDirectory.mkdirs());
+            wallpaperDirectory.mkdirs();
+        }
+
+        try {
+            File f = new File(wallpaperDirectory, Calendar.getInstance()
+                    .getTimeInMillis() + ".jpg");
+            f.createNewFile();   //give read write permission
+            FileOutputStream fo = new FileOutputStream(f);
+            fo.write(bytes.toByteArray());
+            MediaScannerConnection.scanFile(this,
+                    new String[]{f.getPath()},
+                    new String[]{"image/jpeg"}, null);
+            fo.close();
+            Log.d("TAG", "File Saved::--->" + f.getAbsolutePath());
+
+            return f.getAbsolutePath();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+        return "";
+
+    }
+    private Bitmap TextToImageEncode(String Value) throws WriterException {
+        BitMatrix bitMatrix;
+        try {
+            bitMatrix = new MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            );
+
+        } catch (IllegalArgumentException Illegalargumentexception) {
+
+            return null;
+        }
+        int bitMatrixWidth = bitMatrix.getWidth();
+
+        int bitMatrixHeight = bitMatrix.getHeight();
+
+        int[] pixels = new int[bitMatrixWidth * bitMatrixHeight];
+
+        for (int y = 0; y < bitMatrixHeight; y++) {
+            int offset = y * bitMatrixWidth;
+
+            for (int x = 0; x < bitMatrixWidth; x++) {
+
+                pixels[offset + x] = bitMatrix.get(x, y) ?
+                        getResources().getColor(R.color.colorBlack):getResources().getColor(R.color.colorWhite);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444);
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
+        return bitmap;
+    }
 }
